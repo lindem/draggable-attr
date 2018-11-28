81405a17f3591507035fce13a4fc0ea2ff9d868e
timo-lindemann-victors <timo.lindemann@victors.de>
2018-11-28 10:19:42 +0100

    added angle bracket and bare handlebars, and a table showing what's what

--- app/components/dnd-wrapper/component.js
+++ app/components/dnd-wrapper/component.js
@@ -4,6 +4,8 @@ import {computed} from '@ember/object';
 export default Component.extend({
   attributeBindings: ['draggableAttribute:draggable'],
 
+  classNames: ['curly-example'],
+
   draggableAttribute: computed('locked', function() {
     return this.locked ? 'false' : 'true';
   })
